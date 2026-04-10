f37304bf36c880d7c8d0647a1856a7e545a99140
carloscastrox <cacjx84@gmail.com>
2026-04-09 20:54:29 -0500

    Prueba7

--- server/index.js
+++ server/index.js
@@ -1,45 +1,26 @@
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const cors = require("cors");
-// require("dotenv").config({
-//   path: ".env",
-// });
-
-// const authRoutes = require("./routes/auth.routes");
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-
-// app.use("/api/auth", authRoutes);
-
-// mongoose.connect(process.env.MONGO_URI).then(() => {
-//   console.log("MongoDB conectado");
-//   app.listen(4000, () => console.log("Servidor en puerto 4000"));
-// });
-
-
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
-
-const app = express();
-
-app.use(cors({
-  origin: ["https://tf-reactf.vercel.app", "http://localhost:5173"],
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true, // solo si usas cookies
-}));
-
-// Responder preflight
-app.options("*", cors());
-
-app.use(express.json());
-
-// Rutas
-app.post("/api/auth/login", async (req, res) => {
-  return res.json({ ok: true, message: "login endpoint alive" });
+require("dotenv").config({
+  path: ".env",
 });
 
-//  Exporta el handler (esto es lo que Vercel necesita)
-module.exports = app;
+const authRoutes = require("./routes/auth.routes");
+
+const app = express();
+app.use(
+  cors({
+    origin: ["https://tf-reactf.vercel.app", "http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true, // solo si usas cookies
+  }),
+);
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("MongoDB conectado");
+  app.listen(4000, () => console.log("Servidor en puerto 4000"));
+});
