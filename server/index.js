6a38b486ea17c489a44de7e7d8564c02c7cb5ec1
carloscastrox <cacjx84@gmail.com>
2026-04-10 09:34:33 -0500

    Pruebacors

--- server/index.js
+++ server/index.js
@@ -11,8 +11,8 @@ const app = express();
 app.use(
   cors({
     origin: ["https://tf-reactf.vercel.app", "http://localhost:5173"],
-    //methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    //allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // solo si usas cookies
   }),
 );
